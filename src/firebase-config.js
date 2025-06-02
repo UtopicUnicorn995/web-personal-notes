1eee950506645e67f5ce1e3a06c7e59591e96110
utopicUnicorn <christianbarrios995@gmail.com>
2025-06-02 16:43:51 +0800

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1k_ntIBRuj0Liw9C2PDUEfMQLpmTUbaA",
+  authDomain: "personalnotes-f267f.firebaseapp.com",
+  projectId: "personalnotes-f267f",
+  storageBucket: "personalnotes-f267f.firebasestorage.app",
+  messagingSenderId: "1058350876900",
+  appId: "1:1058350876900:web:1ab13426027c661c1544f1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
